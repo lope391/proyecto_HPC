82f94187e21f3048ce4cb4093c0cc43a0c734437
lope c <lope1996@gmail.com>
2017-10-07 19:10:01 -0500

    Quitando stopwords y puntuación

--- serialMain.py
+++ serialMain.py
@@ -1,21 +1,36 @@
 import nltk
 from nltk.stem.snowball import SnowballStemmer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+from nltk.corpus import stopwords
+
+#nltk.download('punkt')
+#nltk.download('stopwords')
+
 
 stemmer = SnowballStemmer("english")
+stWords = set(stopwords.words('english'))
+print(stWords)
 
 file = open("Gutenberg/ShortStory.txt")
 raw = file.read()
 
-words = word_tokenize(raw)
+raw = raw.lower()
+tokenizer = RegexpTokenizer(r'\w+')
+words = tokenizer.tokenize(raw)
+#words = word_tokenize(raw)
 
 print("Palabras originales:")
 print(words)
 
 print("---------------")
+print("Quitando Stopwords en ingles:")
+words_noStop = [wrd for wrd in words if not wrd in stWords]
+print(words_noStop)
+
+print("---------------")
 print("Transformación Snowball Stemmer:")
 
-stems = [stemmer.stem(t) for t in words]
+stems = [stemmer.stem(t) for t in words_noStop]
 print(stems)
 print(len(stems))
 
@@ -29,3 +44,4 @@ print("---------------")
 print("Set de palabras con Pesos:")
 frecuencias = nltk.FreqDist(stems)
 print(frecuencias.tabulate())
+
