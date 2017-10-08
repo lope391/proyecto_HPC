ad1fc7f102bc002f21f2de73492b72cb3d96ffdb
Samuel Sarabia <samuel.sarabia27@hotmail.com>
2017-10-07 20:48:27 -0500

    Refactoring y haciendo funciones

--- serialMain.py
+++ serialMain.py
@@ -11,37 +11,43 @@ stemmer = SnowballStemmer("english")
 stWords = set(stopwords.words('english'))
 print(stWords)
 
-file = open("Gutenberg/ShortStory.txt")
-raw = file.read()
+def leerTexto(nombre):
+    file = open(nombre)
+    raw = file.read()
+    raw = raw.lower()
+    file.close()
+    return raw
 
-raw = raw.lower()
+def arreglar(palabras):
+    print("---------------")
+    print("Quitando Stopwords en ingles:")
+    words_noStop = [wrd for wrd in palabras if not wrd in stWords]
+    print(words_noStop)
+    print("---------------")
+    print("Transformación Snowball Stemmer:")
+    stems = [stemmer.stem(t) for t in words_noStop]
+    print(stems)
+    return(stems)
+
+def palabrasTotales(lista1, lista2):
+    all_words = list(set(lista1).union(set(lista2)))
+    return all_words
+
+texto1 = leerTexto("Gutenberg/ShortStory.txt")
+texto2 = leerTexto("Gutenberg/ShortStory2.txt")
+
 tokenizer = RegexpTokenizer(r'\w+')
-words = tokenizer.tokenize(raw)
+words1 = tokenizer.tokenize(texto1)
+words2 = tokenizer.tokenize(texto2)
+
+words1 = arreglar(words1)
+words2 = arreglar(words2)
 #words = word_tokenize(raw)
 
-print("Palabras originales:")
-print(words)
+print("Palabras originales de la lista 1:")
+print(words1)
+print("Palabras originales de la lista 2:")
+print(words2)
 
-print("---------------")
-print("Quitando Stopwords en ingles:")
-words_noStop = [wrd for wrd in words if not wrd in stWords]
-print(words_noStop)
-
-print("---------------")
-print("Transformación Snowball Stemmer:")
-
-stems = [stemmer.stem(t) for t in words_noStop]
-print(stems)
-print(len(stems))
-
-print("---------------")
-print("Set de palabras:")
-stemSet = set(stems)
-print(stemSet)
-print(len(stemSet))
-
-print("---------------")
-print("Set de palabras con Pesos:")
-frecuencias = nltk.FreqDist(stems)
-print(frecuencias.tabulate())
-
+print("Set de palabras totales")
+print(palabrasTotales(words1,words2))
